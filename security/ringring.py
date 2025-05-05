2f74f857a84e1bbd4d7e8c7f238a31bf296187c1
Walter Lamagna <wlamagna@gmail.com>
2025-05-05 18:11:42 -0300

    Update ringring.py

--- security/ringring.py
+++ security/ringring.py
@@ -1,1 +1,5 @@
-
+# 5-may-25
+# Since i have free time right now while i find my next employee i am playing a bit with Python and
+# my new cameras.  The system is nice but as usual i find things it can not do and i thought that it
+# would be great idea to implement it.  But which is that idea ?
+#
